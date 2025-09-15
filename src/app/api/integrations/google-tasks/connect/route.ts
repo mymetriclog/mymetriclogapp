1799bbdc14fb062b88ce4cf507d21e57e7f9796d
Asad Chaudhary <98808825+asadchaudhary79@users.noreply.github.com>
2025-09-15 18:58:18 +0500

    Update route.ts

--- src/app/api/integrations/google-tasks/connect/route.ts
+++ src/app/api/integrations/google-tasks/connect/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   try {
     const clientId = process.env.GOOGLE_CLIENT_ID;
-    const redirectUri = process.env.GOOGLE_TASK_REDIRECT_URL || ``;
+    const redirectUri = process.env.GOOGLE_TASK_REDIRECT_URL || `${process.env.NEXT_PUBLIC_APP_URL}/api/integrations/google-tasks/callback`;
     const scope = "https://www.googleapis.com/auth/tasks";
     const responseType = "code";
 
